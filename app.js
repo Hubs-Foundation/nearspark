e9be9f024edbcd1ffcc847583f0479bcdb9496fd
Geng Tan <geng.tan@limeade.com>
2022-04-23 15:00:00 -0400

    feature/turkey

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
 const lambda = require("./index");
-var express = require('express');
+const express = require("express");
 
-var app = express();
+const app = express();
 
 // app.get('/_healthz', function (req, res) {
 //   res.send('1');
 // });
 
+app.get("/thumbnail/:urlInput", function(req, res) {
+  console.log(req.url);
+  console.log(req.query);
 
-app.get('/thumbnail/:urlInput', function (req, res) {
-    console.log(req.url)
-    console.log(req.query)
-    
-    req.url=req.url.replace("/thumbnail/","")
+  lambda.handler(
+    {
+      queryStringParameters: req.query,
+      pathParameters: {
+        url: req.url.replace("/thumbnail/", "")
+      }
+    },
+    null,
+    async function(something, callback) {
+      console.log("callback: ", callback);
+      res
+        .status(callback.statusCode)
+        .header(callback.headers)
+        .send(callback.body);
+    }
+  );
+});
+app.get("/thumbnail", function(req, res) {
+  console.log(req.url);
+  console.log(req.query);
 
-    lambda.handler(
-        {queryStringParameters: req.query}, 
-        null,
-        async function (something, callback){
-            console.log("callback: ", callback)
-            res.status(callback.statusCode).header(callback.headers).send(callback.body)
-        }
-    )
-  })
-
-
-app.listen(5000, function () {
-console.log('listening on :5000');
-})
+  lambda.handler(
+    {
+      queryStringParameters: req.query,
+      pathParameters: {
+        url: req.url.replace("/thumbnail", "")
+      }
+    },
+    null,
+    async function(something, callback) {
+      console.log("callback: ", callback);
+      res
+        .status(callback.statusCode)
+        .header(callback.headers)
+        .send(callback.body);
+    }
+  );
+});
+app.listen(5000, function() {
+  console.log("listening on :5000");
+});
