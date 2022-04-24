bad7a9cc4e9d761125a20d8604ca36316e4dc22f
Geng Tan <tanfarming@hotmail.com>
2022-04-24 00:24:25 -0400

    updates for turkey

--- app.js
+++ app.js
@@ -20,33 +20,34 @@ app.get("/thumbnail/:b64url", function(req, res) {
     null,
     async function(something, callback) {
       console.log("callback: ", callback.statusCode, callback.headers);
-      res
-        .status(callback.statusCode)
-        .header(callback.headers)
-        .send(callback.body)
-    }
-  );
-});
-app.get("/thumbnail", function(req, res) {
-  console.log("req.url",req.url);
 
-  lambda.handler(
-    {
-      queryStringParameters: req.query,
-      pathParameters: {
-        url: req.url.replace("/thumbnail", "")
-      }
-    },
-    null,
-    async function(something, callback) {
-      console.log("callback: ", callback.statusCode, callback.headers);
       res
         .status(callback.statusCode)
         .header(callback.headers)
-        .send(callback.body)
+        .send(Buffer.from(callback.body, 'base64'))
     }
   );
 });
+// app.get("/thumbnail", function(req, res) {
+//   console.log("req.url",req.url);
+
+//   lambda.handler(
+//     {
+//       queryStringParameters: req.query,
+//       pathParameters: {
+//         url: req.url.replace("/thumbnail", "")
+//       }
+//     },
+//     null,
+//     async function(something, callback) {
+//       console.log("callback: ", callback.statusCode, callback.headers);
+//       res
+//         .status(callback.statusCode)
+//         .header(callback.headers)
+//         .send(callback.body)
+//     }
+//   );
+// });
 app.listen(5000, function() {
   console.log("listening on :5000");
 });
